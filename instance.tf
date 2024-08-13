7b7552e632d02040f0e1cde909a472ae5ea5d52b
hasnain-eng <73886821+hasnain-eng@users.noreply.github.com>
2024-08-13 18:05:59 +0500

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,20 +1,12 @@
-
 #Creating EC2 instance
 provider "aws" {
-  region     = "us-east-1"
-  }
-resource "aws_instance" "testserver" {
-  ami           = "ami-04df9ee4d3dfde202"
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.keypair.key_name
-  tags = {
-  Name          = "Made via terraform"
-  vpc_security_group_ids = "${aws_security_group.terraform_security_group.id}"
-  }
- }
-
-output "securitygroupDetails" {
-  value = aws_security_group.terraform_security_group.id
+    region = "us-east-1"  
 }
 
-
+resource "aws_instance" "foo" {
+  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  instance_type = "t2.micro"
+  tags = {
+      Name = "TF-Instance"
+  }
+}
