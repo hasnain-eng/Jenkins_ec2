2add9efd7b96335a8e9030e44406882c99ee571b
hasnain-eng <73886821+hasnain-eng@users.noreply.github.com>
2024-08-21 14:01:59 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,6 @@
 #Creating EC2 instance
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAYS2NWNMYNAYFXJVS"
-  secret_key = "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"
 }
 
 resource "aws_instance" "testserver" {
