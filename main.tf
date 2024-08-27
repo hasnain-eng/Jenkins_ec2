74e95f91f2ebcde8aedb915ebf0c6fa57a977fd7
hasnain-eng <73886821+hasnain-eng@users.noreply.github.com>
2024-08-27 17:42:45 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,17 +6,17 @@ provider "aws" {
 resource "aws_instance" "testserver" {
   ami           = "ami-04df9ee4d3dfde202"
   instance_type = "t2.small"
-  key_name      = "test-key"
+  key_name      = aws_key_pair.keypair.key_name
   tags = {
   Name          = "Made via terraform"
   vpc_security_group_ids = "${aws_security_group.terraform_security_group.id}"
   }
 }
 
-#resource "aws_key_pair" "keypair" {
-#  key_name   = "test-key"
-#  public_key = file("${path.module}/public key")
-#}
+resource "aws_key_pair" "keypair" {
+  key_name   = "test-key"
+  public_key = file("${path.module}/public key")
+}
 
 resource "aws_security_group" "terraform_security_group" {
   name        = "my-sg"
